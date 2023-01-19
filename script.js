2934dda043aa2b39ab5390ebb1cae3c6aafbe97d
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-01-19 11:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // Your JS code here.
+<script>
+    // Get all the images on the page
+    const images = document.querySelectorAll(".image");
+
+    // Function to add the active classname to the image when scrolled to
+	  function checkSlide() {
+    // select all images with classname "slide-in"
+    var images = document.querySelectorAll(".slide-in");
+
+    // loop through each image
+    images.forEach(function(image) {
+      // check if the image is in view
+      var slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+      var imageBottom = image.offsetTop + image.height;
+      var isHalfShown = slideInAt > image.offsetTop;
+      var isNotScrolledPast = window.scrollY < imageBottom;
+
+      // if the image is in view, add the active classname
+      if (isHalfShown && isNotScrolledPast) {
+        image.classList.add("active");
+      } else {
+        image.classList.remove("active");
+      }
+    });
+  }
+
+  // pass checkSlide function to debounce function
+  var slideInOnScroll = debounce(checkSlide);
+
+  // listen for scroll event on the window
+  window.addEventListener("scroll", slideInOnScroll);
