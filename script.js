441036f6e911e718b5a8c509ab1476f18509848a
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-01-19 11:03:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-// Your JS code here.
 <script>
     // Get all the images on the page
     const images = document.querySelectorAll(".image");
 
     // Function to add the active classname to the image when scrolled to
-	  function checkSlide() {
-    // select all images with classname "slide-in"
-    var images = document.querySelectorAll(".slide-in");
+    function addActiveClassOnScroll() {
+        images.forEach(image => {
+            // Get the position of the image on the page
+            const imagePosition = image.getBoundingClientRect().top;
+            // Check if the position of the image is within the viewable area of the screen
+            const screenPosition = window.innerHeight / 1.3;
+            if (imagePosition < screenPosition) {
+                image.classList.add("active");
+            }
+        });
+    }
+    // Debounce the addActiveClassOnScroll function
+    const debouncedAddActiveClassOnScroll = debounce(addActiveClassOnScroll, 20);
 
-    // loop through each image
-    images.forEach(function(image) {
-      // check if the image is in view
-      var slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
-      var imageBottom = image.offsetTop + image.height;
-      var isHalfShown = slideInAt > image.offsetTop;
-      var isNotScrolledPast = window.scrollY < imageBottom;
-
-      // if the image is in view, add the active classname
-      if (isHalfShown && isNotScrolledPast) {
-        image.classList.add("active");
-      } else {
-        image.classList.remove("active");
-      }
-    });
-  }
-
-  // pass checkSlide function to debounce function
-  var slideInOnScroll = debounce(checkSlide);
-
-  // listen for scroll event on the window
-  window.addEventListener("scroll", slideInOnScroll);
+    // Listen for scroll events on the window and call the debounced function
+    window.addEventListener("scroll", debouncedAddActiveClassOnScroll);
+</script>
